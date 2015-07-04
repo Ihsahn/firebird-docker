2c759b0e4b5b108ab704c2fc31b2b99f7499121d
Ihsahn <ihsiak@gmail.com>
2015-07-04 20:48:16 +0200

    Wait for fbserver to start instead hardcoded sleep

--- firebird-docker-entrypoint.sh
+++ firebird-docker-entrypoint.sh
@@ -8,9 +8,9 @@ echo "Start fbguard"
 if [ "$FIREBIRD_PASSWORD" ]; then
     echo "New password providerd, altering"
 
-    echo "Wait until Firebird server starts (2 sec)"
+    echo "Wait until Firebird server starts (fbserver process)"
 
-    sleep 2
+    while ! ps aux |grep -v grep| grep fbserver; do echo "."; sleep 1; done ;
 
     OrigPasswd=`grep -oP 'ISC_PASSWD=\K.*' /opt/firebird/SYSDBA.password`
     /opt/firebird/bin/gsec -user sysdba -password $OrigPasswd -modify sysdba -pw $FIREBIRD_PASSWORD
